ee5c3c8581921402d7255af1b7c7d72c80d033cc
Maxim Topchu <maximtop@gmail.com>
2018-10-18 12:03:23 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const data = 'data.json';
-const secondData = 'data.json';
+const dataUrl = 'src/data.json';
+const secondDataUrl= 'src/second-data.json';
+
+const dataElement = document.querySelector('#dataName');
+const secondDataElement = document.querySelector('#secondDataName');
 
 const updateData = async() => {
-  const response = await fetch(data);
-  console.log(response.json());
+  const data = await fetch(dataUrl).then(response => response.json());
 
-  const response2 = await fetch(secondData);
-  console.log(response2.json());
+  const secondData = await fetch(secondDataUrl).then(response => response.json());
+
+  dataElement.textContent = JSON.stringify(data);
+  secondDataElement.textContent = JSON.stringify(secondData);
 };
 
 updateData();
 
 
-// const dataElement = document.querySelector('#dataName');
-// const secondDataElement = document.querySelector('#secondDataName');
-//
-// dataElement.textContent = JSON.stringify(data);
-// secondDataElement.textContent = JSON.stringify(secondData);
+
