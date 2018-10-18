47e33391acd904b39215b8acdab85ded2b15c7fd
Maxim Topchu <maximtop@gmail.com>
2018-10-18 11:48:13 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const data = reqiure('./data.json');
+const data = require('./data.json');
 const secondData = require('./second-data.json');
 
 const dataElement = document.querySelector('#dataName');
 const secondDataElement = document.querySelector('#secondDataName');
 
-dataElement.textContent = data;
-secondDataElement.textContent = secondData;
+dataElement.textContent = JSON.stringify(data);
+secondDataElement.textContent = JSON.stringify(secondData);
